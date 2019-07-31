5fe22a76fcbf6003c6071d480e6617035451c940
mailsonytom@gmail.com <mailsonytom@gmail.com>
2019-07-31 10:37:09 +0530

    change code add connect file

--- connect.php
+++ connect.php
@@ -2,11 +2,7 @@
 $servername = "127.0.0.1";
 $username = "root";
 $password = "password";
-<<<<<<< HEAD
-$dbname = "lawyer";
-=======
 $dbname = "todo_list";
->>>>>>> cdc38375574ff869219774eaa9f4112f9c4ba735
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
