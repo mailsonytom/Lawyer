f6435f540e0a14d9b20490e300fcb30290b4ff04
Sony <mailsonytom@gmail.com>
2019-11-05 13:19:01 +0530

    includes db

--- lawyer.sql
+++ lawyer.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 29, 2019 at 04:35 PM
+-- Generation Time: Nov 05, 2019 at 08:48 AM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
@@ -68,7 +68,8 @@ INSERT INTO `cases` (`case_id`, `description`, `casetype_id`, `uid`, `lid`, `cid
 (3, '                                nlsvcn ladv oadv ', 1, 1, 1, 1, '2019-09-11', 1),
 (4, '                                nlsdhv lsdh lv', 1, 1, 1, 1, '2019-09-04', 1),
 (5, '                                sd;lihabd;lv', 1, 1, 1, 1, '2019-09-18', 1),
-(6, '                                heloo', 1, 1, 3, 1, '2019-09-25', 0);
+(6, '                                heloo', 1, 1, 3, 1, '2019-09-25', 1),
+(7, '                                something fishy', 1, 10, 1, 1, '2019-11-11', 1);
 
 -- --------------------------------------------------------
 
@@ -115,7 +116,8 @@ INSERT INTO `comments` (`com_id`, `case_id`, `user`, `comment`) VALUES
 (532, 2, 0, '                                this comment\r\n'),
 (533, 1, 0, '                                hello'),
 (534, 1, 1, '                                axnvz;'),
-(535, 1, 0, '                                axnvz;');
+(535, 1, 0, '                                axnvz;'),
+(536, 7, 0, '                                Case started');
 
 -- --------------------------------------------------------
 
@@ -197,7 +199,9 @@ CREATE TABLE `lawyer_details` (
 
 INSERT INTO `lawyer_details` (`lid`, `name`, `email`, `password`, `speciality`, `experience`, `fees`, `phone`, `gender`, `dob`, `approved`) VALUES
 (1, 'Sony', 'mailsonytom@gmail.com', '$2y$10$9mhcf7RF2s8fwhMzvmQ9qOsqRDM7H8bPMmMLcULg4VY36bGZS/0Re', 'Criminal Case', 'two years', '10000', '8078518030', 'male', '13/08/1999', 1),
-(3, 'Tony', 'tom@gmail.com', '$2y$10$vmrO5Pyy3zEXQ7xHRyYOh.QMB54MMUOLxPUPHIo698h.kNqNWOF8i', 'Civil case', '4yrs ', '10000', '90987987687', 'Male', '09/90/1990', 1);
+(3, 'Tony', 'tom@gmail.com', '$2y$10$vmrO5Pyy3zEXQ7xHRyYOh.QMB54MMUOLxPUPHIo698h.kNqNWOF8i', 'Civil case', '4yrs ', '10000', '90987987687', 'Male', '09/90/1990', 1),
+(4, 'Rince philip', 'rince12@gmail.com', '$2y$10$uZzYiXsuBkVA6zAi3f9qouK..swGLwNJeDB0ci8o/RY2FPMUxdLnK', 'Civil case', '4 ', '', '', 'Male', '', 1),
+(5, 'Rince philip', 'rince123@gmail.com', '$2y$10$NGuPxI2HQzlWHcBdC9IUDe7T9wAh3w7.yZHiWbPao1s9ltMbCIeY2', 'Civil case', '4yrs ', '10000', '9293456716', 'Male', '1986-11-11', 1);
 
 -- --------------------------------------------------------
 
@@ -218,7 +222,9 @@ CREATE TABLE `user_details` (
 --
 
 INSERT INTO `user_details` (`uid`, `name`, `email`, `password`, `phone`) VALUES
-(1, 'Saran Sasi', 'saransasi@gmail.com', '$2y$10$URGHXgnIg.vOPx.npv91xOOtfJyRDQPdjuU/tAvGPXQu27KoyJdPe', '9009899876');
+(1, 'Saran Sasi', 'saransasi@gmail.com', '$2y$10$URGHXgnIg.vOPx.npv91xOOtfJyRDQPdjuU/tAvGPXQu27KoyJdPe', '9009899876'),
+(9, 'Sony', 'sony222@gmail.com', '$2y$10$yavL7D1O.zaSV1Ls32NYh.dqftxxoVMYq6bj9Y7/8HAi5BtwCcIpC', '0987654432'),
+(10, 'Subin', 'subin12@gmail.com', '$2y$10$s5n26ecD9qgl28NRfAIp7O/jrXcQJTzDx0k99p8zbf2K9VhGjTnAK', '9899767691');
 
 --
 -- Indexes for dumped tables
@@ -286,7 +292,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `cases`
 --
 ALTER TABLE `cases`
-  MODIFY `case_id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `case_id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `casetype`
@@ -298,7 +304,7 @@ ALTER TABLE `casetype`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `com_id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=536;
+  MODIFY `com_id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=537;
 
 --
 -- AUTO_INCREMENT for table `courts`
@@ -316,13 +322,13 @@ ALTER TABLE `history`
 -- AUTO_INCREMENT for table `lawyer_details`
 --
 ALTER TABLE `lawyer_details`
-  MODIFY `lid` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `lid` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `user_details`
 --
 ALTER TABLE `user_details`
-  MODIFY `uid` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `uid` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
