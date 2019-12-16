7fdeaf6126bbb72994efc927bbea52edda389a7d
Sony <mailsonytom@gmail.com>
2019-12-16 22:35:24 +0530

    Include database

--- lawyer.sql
+++ lawyer.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 15, 2019 at 05:58 PM
+-- Generation Time: Dec 16, 2019 at 06:04 PM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
@@ -64,7 +64,11 @@ CREATE TABLE `cases` (
 
 INSERT INTO `cases` (`case_id`, `description`, `casetype_id`, `uid`, `lid`, `cid`, `date`, `active_status`) VALUES
 (24, 'I have a problem with my family property', 24, 10, 5, 35, '2019-12-16', 1),
-(25, 'New case', 20, 10, 5, 31, '2019-12-19', 0);
+(25, 'New case', 20, 10, 5, 31, '2019-12-19', 0),
+(27, 'Problem with my tax.', 23, 20, 5, 32, '2019-12-26', 1),
+(28, 'Problems on my family property', 24, 21, 19, 35, '2019-12-16', 1),
+(29, 'A criminal case on me can you help me out.', 21, 21, 17, 34, '2019-12-21', 0),
+(30, 'A case based on an accident i have met with.', 21, 19, 22, 34, '2019-12-31', 1);
 
 -- --------------------------------------------------------
 
@@ -107,7 +111,10 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`com_id`, `case_id`, `user`, `comment`) VALUES
-(543, 24, 1, 'Hello subin');
+(543, 24, 1, 'Hello subin'),
+(544, 30, 1, 'Hello'),
+(545, 28, 1, 'Hey, what about your case?'),
+(546, 27, 0, 'Heyyy');
 
 -- --------------------------------------------------------
 
@@ -179,7 +186,13 @@ CREATE TABLE `lawyer_details` (
 
 INSERT INTO `lawyer_details` (`lid`, `name`, `email`, `password`, `speciality`, `experience`, `fees`, `phone`, `gender`, `dob`, `approved`) VALUES
 (5, 'Rince philip', 'rince123@gmail.com', '$2y$10$NGuPxI2HQzlWHcBdC9IUDe7T9wAh3w7.yZHiWbPao1s9ltMbCIeY2', 'Civil case', '4yrs ', '10000', '9293456716', 'Male', '1986-11-11', 1),
-(16, 'Neena Thomas', 'neenat@gmail.com', '$2y$10$M34WdVNUOD5JEp9EfKxSbulTRKwXGCN1UV45ya9MsD0EqjgZVgq1C', '22', '3 ', '4000', '9870865678', 'Female', '1995-01-18', 0);
+(17, 'Abraham John Mathew', 'abrahamjm@gmail.com', '$2y$10$WDDKDX.7CGcUZ/ygsUMoTOKEYQxbTzMIoQahRaCNClsEbLD67zXka', 'Criminal case', '13 ', '5000', '9875640987', 'Male', '1964-01-28', 1),
+(18, 'David Sebastian', 'davids@gmail.com', '$2y$10$CLnwnYWFrVm5pefOLb7nhevhOT1qnprJkoJMAG/23VlG.Su7Pn9c6', 'Enforcement case', '8 ', '4500', '7654789643', 'Male', '1980-12-17', 0),
+(19, 'Benny Joseph', 'bennyj@gmail.com', '$2y$10$cyb1IUMenzZYseHmlBJOwOF5xqemXf3V7/x5i3gUS7.WAo.Z6wkz2', 'Property case', '10 ', '3000', '9823090923', 'Male', '1973-02-28', 1),
+(20, 'Varkey Cherian', 'varkeyc@gmail.com', '$2y$10$nHpco2jlGbSNdPyAGNVvH.K0CqMeSCD..aM8Z99kiAVPNFJdU.nYu', 'Civil case', '7 ', '5000', '9098907631', 'Male', '1982-10-20', 0),
+(21, 'Neena Thomas', 'neenat@gmail.com', '$2y$10$n4tjp1wJAiA3qkkCCb8fdek7pwGfSR/iCSlkBfRTh76Pd/2dxgt4y', 'Family case', '3 ', '3000', '9899807234', 'Female', '1990-11-21', 0),
+(22, 'Deepthi S', 'deepthis@gmail.com', '$2y$10$U7mEDPSAevuHYZgMZP44SuyTltBfCGVYo1ULRipQ9MpKExOkxqXJy', 'Criminal case', '5 ', '4000', '7239419023', 'Female', '1983-07-01', 1),
+(23, 'Sunny Abraham', 'sunnya@gmail.com', '$2y$10$POnkwbPw0zUlEku0gYRqL.39Sil.gIupDMftsaUo/Js3ZnaJfvv1m', 'Criminal case', '8 ', '4000', '9087676368', 'Male', '1978-03-15', 1);
 
 -- --------------------------------------------------------
 
@@ -202,7 +215,9 @@ CREATE TABLE `user_details` (
 INSERT INTO `user_details` (`uid`, `name`, `email`, `password`, `phone`) VALUES
 (10, 'Subin', 'subin12@gmail.com', '$2y$10$s5n26ecD9qgl28NRfAIp7O/jrXcQJTzDx0k99p8zbf2K9VhGjTnAK', '9899767691'),
 (19, 'Mathew Varkey', 'mathewv@gmail.com', '$2y$10$Z/iX/TiSYSXTBmoTIp9aJeICjQjnntUSGqcRKx7qeuUZ5Eu1pPMai', '9867547809'),
-(20, 'Arun Jacob', 'arunj@gmail.com', '$2y$10$Vklwe55bNxGFohU.ZKgnfOMX2FxfKjKU8qyuJvtKsH65Hrt/qGyia', '8098767789');
+(20, 'Arun Jacob', 'arunj@gmail.com', '$2y$10$Vklwe55bNxGFohU.ZKgnfOMX2FxfKjKU8qyuJvtKsH65Hrt/qGyia', '8098767789'),
+(21, 'Binu Joseph', 'binuj@gmail.com', '$2y$10$/IYXxgBLzMnCk7Io6QuhsujrgxzoSX3wzaN3YqRuehCejg9b33Z42', '9870090013'),
+(22, 'Johny Kurian', 'johnyk@gmail.com', '$2y$10$hq1XtaEcKP1xWvZ/dDrT8OIiiRxb5kbIXI6rJRxpz0fYORXtC4G9m', '8788990987');
 
 --
 -- Indexes for dumped tables
@@ -277,7 +292,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `cases`
 --
 ALTER TABLE `cases`
-  MODIFY `case_id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `case_id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `casetype`
@@ -289,7 +304,7 @@ ALTER TABLE `casetype`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `com_id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=544;
+  MODIFY `com_id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=547;
 
 --
 -- AUTO_INCREMENT for table `courts`
@@ -307,13 +322,13 @@ ALTER TABLE `history`
 -- AUTO_INCREMENT for table `lawyer_details`
 --
 ALTER TABLE `lawyer_details`
-  MODIFY `lid` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `lid` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `user_details`
 --
 ALTER TABLE `user_details`
-  MODIFY `uid` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `uid` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- Constraints for dumped tables
